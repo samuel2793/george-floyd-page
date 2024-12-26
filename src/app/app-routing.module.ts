28676dd5d29fcf9b64803f2012d471caa97a48b2
samuel2793 <padillasamuel2793@gmail.com>
2024-12-26 23:10:41 +0100

    build: lista fotos component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaFotosComponent } from './lista-fotos/lista-fotos.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/fotos', pathMatch: 'full' },
+  { path: 'fotos', component: ListaFotosComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaFotosComponent } from './lista-fotos/lista-fotos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaFotosComponent
   ],
   imports: [
     BrowserModule,
