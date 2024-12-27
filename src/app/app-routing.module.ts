584cad05ccc07b7b607577e06bcc4ae52782672b
samuel2793 <padillasamuel2793@gmail.com>
2024-12-27 15:49:11 +0100

    chore: testing homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaFotosComponent } from './views/lista-fotos/lista-fotos.component';
+import { HomeComponentComponent } from './views/home-component/home-component.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponentComponent },
   { path: 'fotos', component: ListaFotosComponent },
 ];
 @NgModule({
