9e18cbe63468d5b6b7bf0d7f51c64462a3aacbda
samuel2793 <padillasamuel2793@gmail.com>
2024-12-27 15:34:43 +0100

    feat: added 'home-component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaFotosComponent } from './views/lista-fotos/lista-fotos.component';
 import { PhotoComponentComponent } from './shared/photo-component/photo-component.component';
+import { HomeComponentComponent } from './views/home-component/home-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaFotosComponent,
-    PhotoComponentComponent
+    PhotoComponentComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
