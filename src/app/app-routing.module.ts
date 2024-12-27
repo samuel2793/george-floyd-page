6bab1e54530daffeb8acf513e8d8d8dfb1337868
samuel2793 <padillasamuel2793@gmail.com>
2024-12-27 15:15:53 +0100

    refactor: changed location 'lista-fotos'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListaFotosComponent } from './lista-fotos/lista-fotos.component';
+import { ListaFotosComponent } from './views/lista-fotos/lista-fotos.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/fotos', pathMatch: 'full' },
-  { path: 'fotos', component: ListaFotosComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'fotos', component: ListaFotosComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListaFotosComponent } from './lista-fotos/lista-fotos.component';
+import { ListaFotosComponent } from './views/lista-fotos/lista-fotos.component';
 
 @NgModule({
   declarations: [
